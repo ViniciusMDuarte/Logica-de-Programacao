3360ff97cbd26b73ca8e4b0c82fda6d756e09ae4
Vinicius Duarte <viniduarte222@gmail.com>
2019-05-26 17:30:17 -0300

    Delete lib.js

--- lib.js
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8">           
-function pulaLinha(frase){
-    document.write("<br>")
-}
-function mostra(frase){
-    document.write(frase)
-    pulaLinha();
-}
-console.log('Olá Mundo')
